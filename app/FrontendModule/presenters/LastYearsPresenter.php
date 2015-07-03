b6bfca0de63bfc53d58f03e0a27d56cf263144e9
Lukas Timko <lukast@fykos.cz>
2015-07-03 12:02:48 +0200

    archive l10n

--- app/FrontendModule/presenters/LastYearsPresenter.php
+++ app/FrontendModule/presenters/LastYearsPresenter.php
@@ -5,5 +5,14 @@ class Frontend_ArchivePresenter extends Frontend_BasePresenter
 	public function renderYear2009() {
 		$this->setPageTitle("2009");
 	}
+        
+//        protected function beforeRender() {
+//            parent::beforeRender();
+//            $this->changeViewByLang();
+//        }
+        
+        public function renderTasks($year) {
+            $this->setView('a' . $year . 'tasks.' . $this->lang);
+        }
 
 }
