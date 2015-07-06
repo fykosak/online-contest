04b14d2494b4433fdd681ca907f72dfdcb70f952
Lukas Timko <lukast@fykos.cz>
2015-07-06 17:54:06 +0200

    temp and log

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 cp document_root/.htaccess.example document_root/.htaccess
 cp app/config/config.local.neon.example app/config/config.local.neon
 
-chmod o+w app/temp
-chmod o+w app/log
+chmod o+w temp
+chmod o+w log
