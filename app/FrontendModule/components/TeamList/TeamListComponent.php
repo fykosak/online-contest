4ae64ceab90a5a0eaae452f1a5ff6a00c6c191e9
Lukas Timko <lukast@fykos.cz>
2017-04-03 19:27:17 +0200

    fix if 0 teams

--- app/FrontendModule/components/TeamList/TeamListComponent.php
+++ app/FrontendModule/components/TeamList/TeamListComponent.php
@@ -7,10 +7,15 @@ class TeamListComponent extends BaseListComponent {
 	protected function beforeRender() {
 		$this->getTemplate()->teams = $this->getSource()->fetchAssoc("category,id_team");
 		$ids = $this->getSource()->fetchPairs("id_team", "id_team");
-		$this->getTemplate()->competitors = Interlos::competitors()->findAll()
-				->where("[id_team] IN %l", $ids)
-				->orderBy("id_school", "name")
-				->fetchAssoc("id_team,id_competitor");
+                if(count($ids) > 0) {
+                    $this->getTemplate()->competitors = Interlos::competitors()->findAll()
+                                    ->where("[id_team] IN %l", $ids)
+                                    ->orderBy("id_school", "name")
+                                    ->fetchAssoc("id_team,id_competitor");
+                }
+                else {
+                    $this->getTemplate()->competitors = [];
+                }
 		$this->getTemplate()->categories = Interlos::teams()->getCategoryNames();
 	}
 
