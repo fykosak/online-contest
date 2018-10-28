cb313eca06a5e370f53182f6c8d16b2ed9364165
Lukas Timko <lukast@fykos.cz>
2018-10-29 00:17:10 +0100

    [task stats] add team seen count

--- resources/db/views.sql
+++ resources/db/views.sql
@@ -248,12 +248,11 @@ CREATE VIEW `view_task_stat` AS
 		MAX(`view_correct_answer`.`inserted`) AS `worst_time`,
 		COUNT(DISTINCT `view_correct_answer`.`id_answer`) AS `count_correct_answer`, -- doesn't make sense for cancelled tasks
 		COUNT(DISTINCT `view_answer`.`id_answer`) - COUNT(DISTINCT `view_correct_answer`.`id_answer`) AS `count_incorrect_answer`, -- doesn't make sense for cancelled tasks
-                COUNT(DISTINCT `task_state`.`id_team`) AS `count_skipped`
-
+		(SELECT COUNT(DISTINCT `task_state`.`id_team`) FROM `task_state` WHERE `task_state`.`id_task` = `view_possibly_available_task`.`id_task` AND `task_state`.`skipped` = 1) AS `count_skipped`,
+        (SELECT COUNT(DISTINCT `group_state`.`id_team`) FROM `group_state` WHERE `group_state`.`id_group` = `view_possibly_available_task`.`id_group` AND `group_state`.`task_counter` >= `view_possibly_available_task`.`number`) AS `count_teams_seen`
 	FROM `view_possibly_available_task`
 	LEFT JOIN `view_answer` USING(`id_task`)
 	LEFT JOIN `view_correct_answer` USING(`id_task`,`id_team`)
-        LEFT JOIN `task_state` ON `task_state`.`id_task` = `view_possibly_available_task`.`id_task` AND `task_state`.`skipped` = 1
 	GROUP BY `view_possibly_available_task`.`id_task`
 	ORDER BY `view_possibly_available_task`.`id_group`, `view_possibly_available_task`.`number`;
 
