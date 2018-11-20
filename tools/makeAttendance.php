70b36e8d0f791503e5d172000f3d8d2b31ccf499
Lukas Timko <lukast@fykos.cz>
2018-11-20 02:54:20 +0100

    [tools] new tools for attendance and rank

---
+++ tools/makeAttendance.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$event_id = 122;
+
+$container = require __DIR__ . '/../app/bootstrap.php';
+$teamsModel = $container->getByType('App\Model\TeamsModel');
+$teams = $teamsModel->findAllWithScore()->fetchAll();
+
+foreach ($teams as $team){
+    $teamId = $team->id_team;
+    $status = $team->activity == 1 ? 'participated' : 'missed';
+    echo "UPDATE e_fyziklani_team SET status='$status' WHERE e_fyziklani_team_id=$teamId;\n";
+}
+
+echo "UPDATE event_participant ep INNER JOIN e_fyziklani_participant efp ON ep.event_participant_id=efp.event_participant_id "
+. "INNER JOIN e_fyziklani_team eft ON eft.e_fyziklani_team_id=efp.e_fyziklani_team_id "
+. "SET ep.status=eft.status WHERE ep.event_id=$event_id;\n";
