cc6a7ba2fcb98114a6a015210cfccd0a97e1c19e
Michal "mišo" Červeňák <miso@fykos.cz>
2020-11-12 20:33:22 +0100

    Add consts & update *po files

--- app/Modules/OrgModule/presenters/NoticeboardPresenter.php
+++ app/Modules/OrgModule/presenters/NoticeboardPresenter.php
@@ -2,13 +2,10 @@
 
 namespace FOL\Modules\OrgModule\Presenters;
 
-use Dibi\Exception;
 use FOL\Model\ORM\NotificationService;
-use Nette\Application\AbortException;
 use Nette\Application\BadRequestException;
-use Nette\Application\Responses\JsonResponse;
+use Nette\Application\ForbiddenRequestException;
 use Nette\Http\Response;
-use Nette\NotSupportedException;
 use NotificationFormComponent;
 
 class NoticeboardPresenter extends BasePresenter {
@@ -19,14 +16,13 @@ class NoticeboardPresenter extends BasePresenter {
         $this->notificationModel = $notificationModel;
     }
 
-
     /**
      * @return void
      * @throws BadRequestException
      */
     public function renderAdd(): void {
         if (!$this->user->isAllowed('noticeboard', 'add')) {
-            $this->error('Nemáte oprávnění pro přidání notifikace.', Response::S403_FORBIDDEN);
+            throw new ForbiddenRequestException('Nemáte oprávnění pro přidání notifikace.', Response::S403_FORBIDDEN);
         }
         $this->setPageTitle(_("Přidat notifikaci"));
     }
