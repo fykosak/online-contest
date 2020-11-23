a493c8742adde6662948edeb32a14735c5d1e727
Štěpán Stenchlák <s.stenchlak@gmail.com>
2020-11-23 15:09:53 +0100

    ResultsPresenter, side panel

--- app/Modules/GameModule/presenters/ResultsPresenter.php
+++ app/Modules/GameModule/presenters/ResultsPresenter.php
@@ -4,7 +4,10 @@ namespace FOL\Modules\GameModule\Presenters;
 
 use Exception;
 use FOL\Model\ORM\TeamsService;
+use Nette\Application\BadRequestException;
+use ResultsComponent;
 use ScoreListComponent;
+use TaskStatsComponent;
 
 class ResultsPresenter extends BasePresenter {
 
@@ -16,18 +19,45 @@ class ResultsPresenter extends BasePresenter {
         $this->teamsService = $teamsService;
     }
 
+    /**
+     * @return void
+     * @throws BadRequestException
+     * @throws \Dibi\Exception
+     */
     protected function beforeRender(): void {
         parent::beforeRender();
+        if (!$this->yearsService->isGameStarted()) {
+            $this->error("Statistiky nejsou dostupné.");
+        }
         $this->getTemplate()->categories = $this->teamsService->getCategoryNames();
     }
 
+    public function renderDefault($display = 'all'): void {
+        $this->setPageTitle(_("Výsledky"));
+        $this->check("results");
+        $this->template->display = $display;
+    }
+
     public function renderDetail(): void {
-        $this->setPageTitle(_('Podrobné výsledky'));
-        $this->check('scoreList');
+        $this->setPageTitle(_("Podrobné výsledky"));
+        $this->check("scoreList");
+    }
+
+    public function renderTasks(): void {
+        $this->setPageTitle(_("Statistika úkolů"));
+        $this->check("taskStats");
+    }
+
+    protected function createComponentResults(): ResultsComponent {
+        return new ResultsComponent($this->getContext());
     }
 
     protected function createComponentScoreList(): ScoreListComponent {
         return new ScoreListComponent($this->getContext());
+    }
+
+    protected function createComponentTaskStats(): TaskStatsComponent {
+        return new TaskStatsComponent($this->getContext());
     }
 
     private function check($componentName): void {
@@ -35,7 +65,7 @@ class ResultsPresenter extends BasePresenter {
             $this->getComponent($componentName);
             $this->getTemplate()->available = true;
         } catch (Exception $e) {
-            $this->flashMessage(_('Statistiky jsou momentálně nedostupné. Pravděpodobně dochází k přepočítávání.'), 'danger');
+            $this->flashMessage(_("Statistiky jsou momentálně nedostupné. Pravděpodobně dochází k přepočítávání."), "danger");
             $this->getTemplate()->available = false;
         }
     }
